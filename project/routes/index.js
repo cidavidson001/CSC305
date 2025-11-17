ea91664fe987d48095f410ef6235cfa74d05e5da
zccarver001-gif <zccarver001@converse.edu>
2025-11-17 09:38:37 -0500

    Update index.js
    Part 2 of project.

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+//---Part 2 Data---
+const students = {
+  "1001": { name: "Manuel Roncero", major: "Mathematics", gpa: 3.4},
+  "1002": { name: "Zackery Carver", major: "Data Science", gpa: 3.2},
+  "1003": { name: "Caleb Davidson", major: "Theatre", gpa: 3.1}, 
+};
+
+const faculty = { 
+  "2001": { name: "Dr. Sorrells", department: "Mathematics"},
+  "2002": { name: "Dr. Brown", department: "Computer Science"},
+  "2003": { name: "Dr. Mangum", department: "Data Science"},
+};
+
+const registrarInfo = {
+  officeHours: "Mon-Fri, 8 AM - 4 PM", 
+  location: "Administration Building, Room 109",
+};
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
@@ -20,11 +39,30 @@ router.post('/submit', function(req, res, next) {
   res.redirect(`/role/${encodeURIComponent(role)}/${encodeURIComponent(id)}`);
 });
 
+//---Part 2 Role Pages---
 /* Render role-specific page that displays the ID*/
 router.get('/role/:role/:id', function(req, res, next) {
   const role = req.params.role;
   const id = req.params.id;
-  res.render('role', { title: `Role: ${role}`, role, id });
+
+  let info = null;
+
+  if (role === "student") {
+    info = students[id] || null;
+  }
+  else if (role === "faculty") {
+    info = faculty[id] || null;
+  }
+  else if (role === "registrar") {
+    info = registrarInfo;
+  }
+  
+  res.render('role', { 
+    title: `Role: ${role}`, 
+    role, 
+    id,
+    info
+  });
 });
 
 module.exports = router;
